c0be942ccf8bc10ee97d0f1bcda3a4809938674f
Becki Srofe <blf_23@yahoo.com>
2017-11-01 10:50:18 -0600

    Add first api request spec

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   resources :users,  only: [:index, :show]
 
   get '/search', to: 'search#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :items, except: [:new, :edit]
+    end
+  end    
 end
---
+++ spec/requests/items_request_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe "Items API" do
+  it "returns a list of all items" do
+    Item.create!(name: "Item1", description: "Something", image_url: "www.google.com")
+    Item.create!(name: "Item2", description: "Something", image_url: "www.google.com")
+    Item.create!(name: "Item3", description: "Something", image_url: "www.google.com")
+    Item.create!(name: "Item4", description: "Something", image_url: "www.google.com")
+
+    get '/api/v1/items'
+
+    items = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(items.count).to eq(4)
+  end
+end
