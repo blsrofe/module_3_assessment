046224419d8148b352bfafc0017da915298022b0
Becki Srofe <blf_23@yahoo.com>
2017-11-01 10:22:18 -0600

    Add initialize to stores, not working yet

--- app/services/store.rb
+++ app/services/store.rb
@@ -1,7 +1,17 @@
 class Store
+  attr_reader :long_name, :store_type, :distance, :phone_number, :city
+
+  def initialize(attrs = {})
+    @long_name = attrs[:stores][:longName]
+    @store_type = attrs[:stores][:storeType]
+    @distance = attrs[:stores][:distance]
+    @phone_number = attrs[:stores][:phone]
+    @city = attrs[:stores][:city]
+  end
 
   def self.find_by_zip(zip)
     raw_stores = BestbuyService.find_by_zip(zip)
+    binding.pry
     raw_stores.map do |store_info|
       Store.new(store_info)
     end
